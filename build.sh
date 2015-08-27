98e58b21d6672dcd196876add7fa3faf536853b2
Ben Russell (300178622) <thematrixeatsyou@gmail.com>
2015-08-27 17:55:13 +1200

    update build script

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@
 export EXE_NAME=boot
 export ISO_NAME=chaintest
 
-echo cc && mipsel-none-elf-gcc -c -fomit-frame-pointer -fno-stack-protector -G1 -O3 -msoft-float -nostdlib -mips1 -march=2000 -o obj/main.o main.c && \
+#echo as-dbg && mipsel-none-elf-as -G1 -o obj/head-dbg.o --defsym _LOADABLE_SIZE=0x800 head.S && \
+#echo ld-dbg && true mipsel-none-elf-gcc -msoft-float -o obj/${EXE_NAME}.dbg obj/head-dbg.o obj/main.o -L/usr/local/mipsel-none-elf/lib/soft-float/ -L/usr/local/lib/gcc/mipsel-none-elf/4.7.0/soft-float/ -lm -lc -lgcc -lnosys && \
+
+echo cc && mipsel-none-elf-gcc -c -fomit-frame-pointer -fno-stack-protector -G1 -O3 -msoft-float -nostdlib -mips1 -march=3000 -o obj/main.o main.c && \
 echo as && mipsel-none-elf-as -G1 -o obj/head.o head.S && \
-echo as-dbg && mipsel-none-elf-as -G1 -o obj/head-dbg.o --defsym _LOADABLE_SIZE=0x800 head.S && \
-echo ld-dbg && true mipsel-none-elf-gcc -msoft-float -o obj/${EXE_NAME}.dbg obj/head-dbg.o obj/main.o -L/usr/local/mipsel-none-elf/lib/soft-float/ -L/usr/local/lib/gcc/mipsel-none-elf/4.7.0/soft-float/ -lm -lc -lgcc -lnosys && \
 echo ld && mipsel-none-elf-ld -T link.ld -o obj/${EXE_NAME}.elf obj/head.o obj/main.o -L/usr/local/mipsel-none-elf/lib/soft-float/ -L/usr/local/lib/gcc/mipsel-none-elf/4.7.0/soft-float/ -lm -lc -lgcc -lnosys && \
-echo strip && mipsel-none-elf-strip -R .reginfo -R .pdr -R .text.startup -R .eh_frame -R .comment -R .gnu.attributes obj/${EXE_NAME}.elf && \
+echo strip && mipsel-none-elf-strip -R .reginfo -R .pdr -R .text.startup -R .eh_frame -R .comment -R .gnu.attributes -R .rel.dyn obj/${EXE_NAME}.elf && \
 echo objcopy && mipsel-none-elf-objcopy -O binary obj/${EXE_NAME}.elf obj/${EXE_NAME}.tmp && \
 cp obj/${EXE_NAME}.tmp ${EXE_NAME}.exe && \
 cc -o tools/iso2raw tools/iso2raw.c && \
